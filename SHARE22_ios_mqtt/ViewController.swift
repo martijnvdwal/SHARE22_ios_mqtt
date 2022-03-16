89a0eb26bb95b2150e09d0b6b74f3d15bcbb28e3
martijnvdwal <martijnvdwal2001@gmail.com>
2022-03-16 09:29:50 +0100

    Initial Commit

---
+++ SHARE22_ios_mqtt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SHARE22_ios_mqtt
+//
+//  Created by martijn van der wal on 16/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
